fb9113d166dc7144a5cd15cf59ed63d5415d8479
cosmicEarth <peterchenn0302@gmail.com>
2024-01-06 13:51:04 +0700

    [main] adjust z-index

--- src/app/components/ModalWrapper/index.tsx
+++ src/app/components/ModalWrapper/index.tsx
@@ -3,7 +3,7 @@ import { createPortal } from "react-dom";
 
 export default function ModalWrapper({ children }: { children: ReactNode }) {
     return createPortal(
-        <div className="min-h-dvh min-w-full max-h-dvh max-w-full absolute flex flex-col top-0 left-0">
+        <div className="min-h-dvh min-w-full max-h-dvh max-w-full absolute flex flex-col top-0 left-0 z-50">
             {children}
         </div>,
         document.getElementById("modal-wrapper")!
