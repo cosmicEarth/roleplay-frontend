f5f8348c9068c67df86f492e7aa81412f6a23e70
cosmicEarth <peterchenn0302@gmail.com>
2024-01-13 21:43:51 +0700

    [main] add validation token is verified or not

--- src/app/login_verify/[token]/route.ts
+++ src/app/login_verify/[token]/route.ts
@@ -1,25 +1,37 @@
 import { AuthSession, updateAuthSession } from "@/lib/authSession";
-import { NextRequest, NextResponse } from "next/server";
+import { magicLinkVerifyService } from "@/lib/magicLinkAction";
+import { NextResponse } from "next/server";
 
-export async function GET(
-    req: NextRequest,
-    { params }: { params: { token: string } }
-) {
-    console.log(params.token);
+export async function GET(req: any, { params }: { params: { token: string } }) {
+    const verifyReq = await magicLinkVerifyService(params.token);
 
-    const newSession: AuthSession = {
-        user: {
-            id: "123213",
-            full_name: "Ersapta Aristo",
-            email: "aristoersapta@gmail.com",
-            profile_image: null,
-            stay_sign: true,
-        },
-        refresher: "abc",
-        access: "def",
-    };
+    if (verifyReq.ok && verifyReq.status === 200) {
+        const data = await verifyReq.json();
+        if (data?.error) {
+            return data?.error;
+        }
+        console.log(data);
+        const newSession: AuthSession = {
+            user: {
+                id: data.data.id,
+                full_name: data.data.full_name,
+                email: data.data.email,
+                profile_image: data.data.profile_image,
+                stay_sign: data.data.stay_sign,
+            },
+            refresher: data.refresher,
+            access: data.access,
+        };
 
-    await updateAuthSession(newSession);
+        await updateAuthSession(newSession);
 
-    return NextResponse.redirect(`${process.env.NEXTAUTH_URL}/chats`);
+        return NextResponse.redirect(`${process.env.NEXTAUTH_URL}/chats`);
+    } else {
+        console.log({ verifyReq });
+
+        return new NextResponse(null, {
+            status: 404,
+            statusText: "Error hehehe",
+        });
+    }
 }
