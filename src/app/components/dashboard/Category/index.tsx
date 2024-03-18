68f614366153c28c44f987097f0785340dba0f54
cosmicEarth <peterchenn0302@gmail.com>
2024-03-18 13:13:46 +0700

    [main] adjust category card

--- src/app/components/dashboard/Category/index.tsx
+++ src/app/components/dashboard/Category/index.tsx
@@ -7,7 +7,7 @@ interface CategoryProps {
 export default function Category({ children, active = false }: CategoryProps) {
     return (
         <div
-            className={`relative px-2 py-2 flex flex-row h-9 items-center justify-center font-semibold cursor-pointer select-none whitespace-nowrap text-xs ${
+            className={`relative px-2 py-2 flex flex-row items-center justify-center font-semibold cursor-pointer select-none whitespace-nowrap text-xs ${
                 active
                     ? "bg-blue-500 text-white-0"
                     : "bg-white-0 text-black-200"
