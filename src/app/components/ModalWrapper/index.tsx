988d546998de7a6cd10e78f06f1321a7dd3744bc
cosmicEarth <peterchenn0302@gmail.com>
2024-03-19 12:30:41 +0700

    [main] adjusting lora edit modal

--- src/app/components/ModalWrapper/index.tsx
+++ src/app/components/ModalWrapper/index.tsx
@@ -12,7 +12,7 @@ export default function ModalWrapper({ children }: { children: ReactNode }) {
     }, []);
 
     return createPortal(
-        <div className="min-h-dvh min-w-full max-h-dvh max-w-full absolute flex flex-col top-0 left-0 z-50 overflow-scroll">
+        <div className="min-h-dvh min-w-full max-h-dvh max-w-full absolute flex flex-col top-0 left-0 z-50">
             {children}
         </div>,
         document.getElementById("modal-wrapper")!
