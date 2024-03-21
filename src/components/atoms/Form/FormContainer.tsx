da7c593a46518b3eef9531841d9f43fd8f8170dd
cosmicEarth <peterchenn0302@gmail.com>
2024-03-22 01:12:09 +0700

    [main] dark mode create lora

--- src/components/atoms/Form/FormContainer.tsx
+++ src/components/atoms/Form/FormContainer.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { HTMLAttributes } from "react";
 
 type FormContainerProps = {
     children: React.ReactNode;
     fullWidth?: boolean;
+    customClass?: HTMLAttributes<HTMLDivElement>["className"];
 };
 
 const FormContainer = (props: FormContainerProps) => {
@@ -10,7 +11,7 @@ const FormContainer = (props: FormContainerProps) => {
         <div
             className={`flex flex-col rounded-lg bg-white-0 dark:bg-black-900 text-black-900 dark:text-white-200 p-4 ${
                 props.fullWidth ? "flex-grow" : ""
-            } `}
+            } ${props.customClass ? props.customClass : ""} `}
         >
             {props.children}
         </div>
